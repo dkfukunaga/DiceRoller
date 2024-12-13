09a0a6f40b6a2defcce6ab81bbbf4fe713eaa384
dkfukunaga <dkfukunaga@gmail.com>
2024-12-13 01:11:48 -0800

    interface and abstract decorator class for die rolls and roll modifiers

---
+++ src/main/java/com/github/dkfukunaga/diceroller/Check.java
@@ -0,0 +1,6 @@
+package com.github.dkfukunaga.diceroller;
+
+public interface Check {
+    String getName();
+    CheckResult getResult();
+}
---
+++ src/main/java/com/github/dkfukunaga/diceroller/CheckModifier.java
@@ -0,0 +1,27 @@
+package com.github.dkfukunaga.diceroller;
+
+public abstract class CheckModifier implements Check {
+    protected Check     baseCheck;
+
+    public CheckModifier(Check baseCheck) {
+        this.baseCheck = baseCheck;
+    }
+
+    @Override
+    public String getName() {
+        return baseCheck.getName();
+    }
+
+    @Override
+    public CheckResult getResult() {
+        return baseCheck.getResult();
+    }
+
+    static protected String formatModifier(int modifier) {
+        return (modifier > 0 ? " +" + modifier : (modifier < 0 ? " " + modifier : ""));
+    }
+
+    static protected String formatBold(int modifier) {
+        return "<b>" + modifier + "</b>";
+    }
+}
