50790802efd59518b1a859e58ddee931cc8468d7
dkfukunaga <dkfukunaga@gmail.com>
2024-12-13 01:13:03 -0800

    class stub

---
+++ src/main/java/com/github/dkfukunaga/diceroller/RollCheck.java
@@ -0,0 +1,13 @@
+package com.github.dkfukunaga.diceroller;
+
+public class RollCheck implements Check {
+    @Override
+    public String getName() {
+        return "";
+    }
+
+    @Override
+    public CheckResult getResult() {
+        return null;
+    }
+}
