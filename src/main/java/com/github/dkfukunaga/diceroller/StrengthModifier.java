85ca0bf12e0d4c7522b877b7ca2b9d6e4e2f9d08
dkfukunaga <dkfukunaga@gmail.com>
2024-12-13 20:05:52 -0800

    renamed and implemented RollResult add methods

---
+++ src/main/java/com/github/dkfukunaga/diceroller/StrengthModifier.java
@@ -0,0 +1,45 @@
+package com.github.dkfukunaga.diceroller;
+
+import javafx.scene.text.Text;
+import javafx.scene.text.TextFlow;
+
+import java.util.Objects;
+
+public class StrengthModifier extends DiceModifier {
+    private final int modifier;
+
+    public StrengthModifier(DiceBase baseDiceBase, int modifier) {
+        super(baseDiceBase);
+        this.modifier = modifier;
+    }
+
+    @Override
+    public String getName() {
+        return diceRoll.getName() + ", Strength";
+    }
+
+    @Override
+    public RollResult getResult() {
+        return diceRoll.getResult().addAll(modifier, new TextFlow(new Text(" " + formatModifier(modifier))));
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof StrengthModifier that)) return false;
+        return modifier == that.modifier;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(modifier);
+    }
+
+    @Override
+    public String toString() {
+        return "StrengthModifier{" +
+                "modifier=" + modifier +
+                ", diceRoll=" + diceRoll +
+                '}';
+    }
+}
