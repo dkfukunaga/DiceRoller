e353c6c2c38a3963bd4dd6deb4527caf88c7a0d6
dkfukunaga <dkfukunaga@gmail.com>
2024-12-13 20:01:17 -0800

    added jetbrains annotation

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module com.github.dkfukunaga.diceroller {
     requires javafx.controls;
     requires javafx.fxml;
+    requires org.jetbrains.annotations;
 
 
     opens com.github.dkfukunaga.diceroller to javafx.fxml;
